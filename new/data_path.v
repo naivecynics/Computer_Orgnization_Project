209217f07007bd657d6fd3bb94be669f931f7bc6
IRONMAN1024 <cctt04@126.com>
2024-05-12 22:39:59 +0800

    datapath finish!

--- new/data_path.v
+++ new/data_path.v
@@ -1,29 +1,19 @@
-`timescale 1ns / 1ps
-
 module data_path(
     input clk,
-    input rst_n,
-    input [31:0] instr,
-    input MemtoReg,
-    input RegWrite,
-    input ALUSrc,
-    input [31:0] R_data,
-    output [6:0] opcode,
-    output [31:0] W_data,
-    output reg [31:0] ALUResult
+    input rst_n
 );
+
+    //instantiating the instruction decoder
 
     wire [2:0] funct3;
     wire [6:0] funct7;
     wire [4:0] rs1;
     wire [4:0] rs2;
     wire [4:0] rd;
-    wire W_en;
     wire [31:0] imme;
     wire [31:0] R_data_1;
     wire [31:0] R_data_2;
 
-    //instantiating the instruction decoder
     instr_decoder instr_decoder_inst(
         .instr(instr),
         .opcode(opcode),
@@ -35,17 +25,40 @@ module data_path(
         .imme(imme)
     );
 
+    // controller module
+
+    wire Branch;
+    wire MemRead;
+    wire MemtoReg;
+    wire RegWrite;
+    wire ALUSrc;
+    wire MemWrite;
+    wire [1:0] ALUOp;
+
+    controller controller_inst(
+        .inst(instr),
+        .Branch(Branch),
+        .MemRead(MemRead),
+        .MemtoReg(MemtoReg),
+        .RegWrite(RegWrite),
+        .ALUSrc(ALUSrc),
+        .MemWrite(MemWrite),
+        .ALUOp(ALUOp)
+    );
+
+    // data memory module
+
     
 
-    wire [31:0] pc_in;
-    wire [31:0] pc_out;
-
     //instantiating the program counter
+    
+    wire [31:0] pc;
+
     pc pc_inst(
         .clk(clk),
         .rst_n(rst_n),
-        .pc_in(pc_in),
-        .pc_out(pc_out)
+        .pc_in(pc),
+        .pc_out(pc)
     );
 
     //instantiating the register file
@@ -56,13 +69,13 @@ module data_path(
         .R_reg_2(rs2),
         .W_reg(rd),
         .W_data(W_data),
-        .W_en(W_en),
+        .W_en(RegWrite),
         .R_data_1(R_data_1),
         .R_data_2(R_data_2)
     );
 
-    wire [1:0] ALUOp;
     //instantiating the ALU
+
     wire zero;
 
     ALU alu_inst(
@@ -77,14 +90,38 @@ module data_path(
         .zero(zero)
     );
 
+    // data memory module
+
+    wire [31:0] ram_data_out;
+    dememory dememory_inst(
+        .ram_clk_i(clk),
+        .ram_wen_i(MemWrite),
+        .ram_adr_i(ALUResult[13:0]),
+        .ram_dat_i(R_data_2),
+        .ram_dat_o(ram_data_out)
+    );
 
 
+    //mux for selecting data to be written to the register file
 
 
+    always @(*) begin
+        if (MemtoReg) begin
+            W_data = ram_data_out;
+        end
+        else begin
+            W_data = ALUResult;
+        end
+    end
 
 
+    // mux for selecting data to ALU operand2
 
+        // ALU mux is already implemented in the ALU module
 
+    // mux for selecting data to PC
+
+        // PC mux is already implemented in the PC module
 
 
 endmodule
--- new/pc.v
+++ new/pc.v
@@ -1,17 +1,26 @@
-`timescale 1ns / 1ps
-
 module pc(
     input clk,
     input rst_n,
+    input Branch,
+    input zero,
+    input [31:0] imm,
     input [31:0] pc_in,
-    output [31:0] pc_out
+    output reg [31:0] pc_out
 );
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin   
             pc_out <= 32'h00000000;
         end else begin
-            pc_out <= pc_in;    
+            // beq instruction
+            // todo: add other instructions & controllers
+            if (Branch && zero) begin
+                pc_out <= pc_in + imm;
+            end 
+            else begin
+                pc_out <= pc_in + 4;
+            end
         end
     end
+
 endmodule
